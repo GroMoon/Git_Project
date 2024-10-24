4d62250d713f8788747ba3bf3669f0c1d06bf1c3
GroMoon <msj99112@gmail.com>
2024-10-24 15:39:35 +0900

    === [COMMON-008] [DEV] 퍼즈 구현

---
+++ SlayerSurvival/dynamic/1_player/UI/BaseUI.gd
@@ -0,0 +1,18 @@
+extends Control
+
+@onready var pause_panel = get_node("PausePanel")
+
+func _ready():
+	# PausePanel 가리기
+	pause_panel.visible = false
+
+func _process(_delta):
+	# esc("pause") 누르면 PausePanel visible 및 모든 노드 중지(PausePanel 제외)
+	if Input.is_action_just_pressed("pause"):
+		pause_panel.visible = true
+		get_tree().paused = true
+
+# resume(돌아가기) 버튼 누를 때
+func _on_resume_pressed():
+	get_tree().paused = false
+	pause_panel.visible = false
