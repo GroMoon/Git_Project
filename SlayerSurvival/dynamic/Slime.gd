8d55c029e38560ed944b8abdd3bedc2318d722f4
GroMoon <msj99112@gmail.com>
2024-11-11 14:17:33 +0900

    === 적 pause 앞으로 나오는거 수정 / knight 경로 오류 수정

--- SlayerSurvival/dynamic/Slime.gd
+++ SlayerSurvival/dynamic/Slime.gd
@@ -19,7 +19,7 @@ var invincibility_flag
 @onready var hp_bar = get_node("BaseUI/HpBar")
 
 # hp 설정 (체력 value 관리)
-var max_hp = 10.0
+var max_hp = 50.0
 var hp = max_hp:	#TODO 왜 변수가 함수처럼 쓰이는지 어떤 경우 그렇게 쓰이는지 확인 필요
 	set(value):
 		hp = value
