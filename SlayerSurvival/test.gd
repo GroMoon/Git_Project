8dcb998e5b960a3bc8e17e777c2cef4b0882f8eb
GroMoon <msj99112@gmail.com>
2024-11-29 17:37:52 +0900

    === 파일 수정

--- SlayerSurvival/test.gd
+++ SlayerSurvival/test.gd
@@ -13,19 +13,8 @@ func _ready():
 func _process(_delta):
 	pass
 
-# 슬라임 버튼 누르면
-func _on_select_slime_pressed():
-	character_load = preload("res://dynamic/slime.tscn")
-	instance_character = character_load.instantiate()
-	instance_character.name = "player"
-	# 스케일 조정
-	instance_character.scale = Vector2(0.5,0.5)
-	add_child(instance_character)
-	get_tree().paused = false
-	select_panel.queue_free()
-
 # Fantasy Warrior
-func _on_select_golem_pressed():
+func _on_select_warrior_pressed():
 	character_load = preload("res://dynamic/1_player/characters/Fantasy_Warrior/fantasy_warrior.tscn")
 	instance_character = character_load.instantiate()
 	instance_character.name = "player"
@@ -35,10 +24,18 @@ func _on_select_golem_pressed():
 	get_tree().paused = false
 	select_panel.queue_free()
 
-# Slime 마우스 올리면
-func _on_select_slime_mouse_entered():
+func _on_select_king_pressed():
+	character_load = preload("res://dynamic/1_player/characters/Medieval_King/medieval_king.tscn")
+	instance_character = character_load.instantiate()
+	instance_character.name = "player"
+	# 스케일 조정
+	instance_character.scale = Vector2(1,1)
+	add_child(instance_character)
+	get_tree().paused = false
+	select_panel.queue_free()
+
+func _on_select_king_mouse_entered():
 	$Button_sound.play()
 
-# Golem 마우스 올리면
-func _on_select_golem_mouse_entered():
+func _on_select_warrior_mouse_entered():
 	$Button_sound.play()
