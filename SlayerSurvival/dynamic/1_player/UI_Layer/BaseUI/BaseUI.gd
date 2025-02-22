c2c1bbe7fcd501ad5a847f29796c238ba55046c6
GroMoon <msj99112@gmail.com>
2025-02-22 12:19:16 +0900

    === [CHARACTER-025] [CR]  피격 시 이펙트 변경

--- SlayerSurvival/dynamic/1_player/UI_Layer/BaseUI/BaseUI.gd
+++ SlayerSurvival/dynamic/1_player/UI_Layer/BaseUI/BaseUI.gd
@@ -4,6 +4,7 @@ extends Control
 @onready var pause_panel = get_node("PausePanel")
 @onready var death_panel = get_node("DeathPanel")
 @onready var stopwatch   = get_node("Stopwatch")
+@onready var fatal_state = $FatalState
 
 var sec                 = 0.0
 var minute              = 0
@@ -14,6 +15,7 @@ var death_pause_flag    = false
 var player = null
 
 func _ready():
+	# esc("pause") 누르면 PausePanel visible 및 모든 노드 중지(PausePanel 제외)
 	pause_panel.visible = false		# PausePanel 가리기
 	death_panel.visible = false		# DeathPanel 가리기
 	select_ui.connect("pause", Callable(self, "check_level_up_pause_flag"))
@@ -23,7 +25,7 @@ func _ready():
 	# print(player.death_flag)
 
 func _process(delta):
-	# esc("pause") 누르면 PausePanel visible 및 모든 노드 중지(PausePanel 제외)
+	process_fatal_state()
 	check_pause_pressed()
 
 	death_pause_flag = player.death_flag
@@ -53,6 +55,15 @@ func _process(delta):
 func check_pause_pressed():
 	if Input.is_action_just_pressed("pause") : 
 		globl_pause_flag = !globl_pause_flag
+
+# fatal_state 효과 (빨간빛)
+func process_fatal_state():
+	var health_ratio = float (player.current_hp) / player.max_hp
+	if health_ratio < 0.3:  # 체력이 30% 미만일 때 효과 적용
+		var intensity = health_ratio
+		fatal_state.material.set_shader_parameter("health", intensity)
+	else:
+		fatal_state.material.set_shader_parameter("health", 1.0)  # 체력이 높을 때 효과 제거
 
 # 레벨 업 시 pause 관련 신호 수신 함수
 func check_level_up_pause_flag(pause_state: bool):
