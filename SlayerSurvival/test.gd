6b25483fdf3f68d8563b0595a340b4fcb369ccfe
GroMoon <msj99112@gmail.com>
2025-03-25 09:36:32 +0900

    === 캐릭터, 맵 설명 추가

--- SlayerSurvival/test.gd
+++ SlayerSurvival/test.gd
@@ -113,7 +113,7 @@ func _on_dungeon_button_pressed():
 func _on_select_warrior_mouse_entered():
 	$Button_sound.play()
 	character_icon.texture = preload("res://dynamic/1_player/selcet_character/character_img/fantasy_warrior_pixelart.webp")
-	character_details.text = "어쩌구 어쩌구"
+	character_details.text = "신비로운 과거를 품은 검객, 긴 백발 속에는 싸움의 세월이 녹아 있으며, 한 손에 쥔 검은 마치 그의 분신과도 같습니다. 그는 적들을 단칼에 베어버리는 강력한 공격력과 독보적인 전투 기술로 전장에서의 전설로 불립니다. 하지만 내면 깊은 곳에는 복수를 꿈꾸는 어둠이 자리 잡고 있습니다."
 	attack_label.text = "■■"
 	move_label.text   = "■■■■"
 	helth_label.text  = "■■■"
@@ -121,19 +121,19 @@ func _on_select_warrior_mouse_entered():
 func _on_select_king_mouse_entered():
 	$Button_sound.play()
 	character_icon.texture = preload("res://dynamic/1_player/selcet_character/character_img/medieval_king_pixelart.webp")
-	character_details.text = "어쩌구 저쩌구"
+	character_details.text = "왕관을 쓴 전사의 모습을 한 그는, 자신의 영토와 백성을 위해 싸우는 불굴의 지도자입니다. 그가 휘두르는 거대한 검은 적에게 공포를 심어주며, 중세의 전장에서 왕으로서의 강력한 존재감을 드러냅니다. 고결한 로브와 왕관은 그의 권력을 상징하며, 모든 적은 그의 분노 앞에서 무릎을 꿇게 됩니다."
 	attack_label.text = "■■■■"
 	move_label.text   = "■■"
 	helth_label.text  = "■■■■■"
 
 func _on_cave_button_mouse_entered():
 	preview_map.texture = preload("res://dynamic/4_world/Cave/cave_title.png")
-	map_details.text = "Cave 맵 설명"
+	map_details.text = "어둠 속에 반짝이는 수정들과 습한 공기가 느껴지는 공간... \n바닥의 구멍을 조심하세요!"
 	$Button_sound.play()
 
 func _on_dungeon_button_mouse_entered():
 	preview_map.texture = preload("res://dynamic/4_world/Dungeon_B1F/dungeon_title.png")
-	map_details.text = "Dungeon 맵 설명"
+	map_details.text = "거대한 돌벽과 낡은 상자들 사이로 퍼지는 섬뜩한 분위기"
 	$Button_sound.play()
 
 # 해당 씬 재시작
