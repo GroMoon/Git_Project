5e2ddbf7a9b4444a681c6ff2794f4dabd9addbbb
GroMoon <msj99112@gmail.com>
2025-04-21 15:41:20 +0900

    === [DEV] options(screenmode) in ingame

---
+++ SlayerSurvival/dynamic/5_title_screen/option/ingame_options.gd
@@ -0,0 +1,32 @@
+extends Panel
+
+@onready var full_screen     = $Screen/screen_mode/full_screen
+@onready var window_screen   = $Screen/screen_mode/window_screen
+
+func _ready():
+	update_ui()
+	pass # Replace with function body.
+
+func update_ui():
+	full_screen.disabled = false
+	window_screen.disabled = false
+
+	full_screen.button_pressed = OptionsManager.is_fullscreen
+	window_screen.button_pressed = !OptionsManager.is_fullscreen
+
+	full_screen.disabled = OptionsManager.is_fullscreen
+	window_screen.disabled = !OptionsManager.is_fullscreen
+
+
+func _on_full_screen_toggled(toggled_on):
+	if toggled_on and !OptionsManager.is_fullscreen:
+		OptionsManager.apply_screen_mode(true)
+		update_ui()
+
+func _on_window_screen_toggled(toggled_on):
+	if toggled_on and OptionsManager.is_fullscreen:
+		OptionsManager.apply_screen_mode(false)
+		update_ui()
+
+func _on_back_pressed():
+	queue_free()
