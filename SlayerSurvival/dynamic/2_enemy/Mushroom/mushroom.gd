b409d61b8035795301af396a6337ab9f56533bef
wonhs0191 <wonhs0191@naver.com>
2025-05-03 12:45:15 +0900

    ^_^ enemy 객체지향 프로그래밍 mushroom 기본 적용

--- SlayerSurvival/dynamic/2_enemy/Mushroom/mushroom.gd
+++ SlayerSurvival/dynamic/2_enemy/Mushroom/mushroom.gd
@@ -1,131 +1,18 @@
-extends CharacterBody2D
+extends EnemyBase
 
-const ANIMATION_SPEED = 1.5		# 기본 애니메이션 속도
-const PET_CHANCE      = 0.5		# 펫이 될 확률
-
-@onready var collision_shape    = $CollisionShape2D
-@onready var animated_sprite    = $AnimatedSprite2D
-@onready var interaction_sensor = $interaction_sensor 
-
-# 아이템
-var gold_img = preload("res://dynamic/6_utillity/items/gold/gold.tscn")
-var exp_img = preload("res://dynamic/6_utillity/items/exp/exp.tscn")
-#var golds = 25
-
-# 적 특성
-var health       = 15 	# 적 체력
-var move_speed   = 80 	# 적 이동 속도
-var damage       = 5  	# 적 데미지
-var spawn_radius = 500  # 스폰 범위
-# 전역 변수
-var player 
-var touch_flag = false 
-var hit_flag   = false
-var is_dead    = false
-# 넉백 관련
-var knockback_vector   = Vector2.ZERO
-var knockback_time     = 0.0			# 넉백 유지 시간
-var knockback_duration = 0.2			# 넉백 몇 초 동안?
-var knockback_strength = 150.0  		# 넉백 세기
+func _initialize():
+	# 적 특성
+	enemy_name      = "mushroom"
+	health          = 15
+	move_speed      = 80
+	damage          = 5
+	spawn_radius    = 500
+	animation_speed = 1.5
+	pet_chance      = 0.9
 
 func _ready():
-	# player 노드 찾기
-	player = get_parent().get_parent().get_node("player")
-	# print("Enemy instance name : ", name)
-	
+	_initialize()
+	super._ready()
+
 func _physics_process(delta):
-	# 사망 상태에서 아무것도 처리 아지 않도록
-	if is_dead:
-		return
-
-	# 넉백 중이면 넉백 로직 우선 적용
-	if knockback_time > 0.0:
-		knockback_time -= delta
-		velocity = knockback_vector
-		move_and_slide()
-		return
-	else:
-		# 넉백 로직 끝날 시 원래 로직으로 복귀귀
-		# 플레이어가 존재하면 플레이어를 향해 이동
-		if player:
-			var direction = (player.position - position).normalized()
-			velocity = direction * move_speed
-			move_and_slide()
-
-		# 애니메이션 처리
-		if (velocity.length() > 0) && (!hit_flag):
-			animated_sprite.speed_scale = ANIMATION_SPEED
-			$AnimatedSprite2D.play("walk")
-			$AnimatedSprite2D.flip_h = velocity.x < 0
-			
-	if touch_flag:
-		player.process_collision_enemy(damage)
-
-# 사망 처리 함수
-func die_enemy():
-	var pet_chance = randf()							# 몬스터펫 확률 (0.0~1.0 사이로 조절)
-	is_dead = true 										# 사망 상태 활성화
-	drop_item()
-	player.kill_count += 1
-	collision_shape.call_deferred("set_disabled",true)	# CollisionShape2D 비활성화
-	interaction_sensor.call_deferred("queue_free")		# interaction_sensor 삭제
-	animated_sprite.play("death")
-	await animated_sprite.animation_finished
-	if (player.is_mushroom_pet==false)&&(pet_chance <= PET_CHANCE):
-		# UI 관련 코드, 몬스터펫 업그레이드 관련 코드
-		player.mushroom_pet = true
-		Dialogic.start("res://dynamic/7_dialogic/get_mushroom_pet.dtl").process_mode = Node.PROCESS_MODE_ALWAYS
-		Dialogic.process_mode = Node.PROCESS_MODE_ALWAYS
-		queue_free()
-	else:
-		queue_free()										# 적 노드 삭제
-
-# 아이템 드랍 함수
-func drop_item():
-	# 골드
-	var gold_chance = randf()
-	if gold_chance <= 0.5:								# 드랍 확률 조정 (0.0~1.0)
-		var new_gold = gold_img.instantiate()
-		new_gold.global_position = global_position
-		get_parent().call_deferred("add_child", new_gold)
-	# 경험치
-	var exp_chance = randf()
-	if exp_chance <= 0.7:								# 드랍 확률 조정 (0.0~1.0)
-		var new_exp = exp_img.instantiate()
-		new_exp.global_position = global_position + Vector2(10, 0)
-		get_parent().call_deferred("add_child", new_exp)
-
-# 넉백 함수
-func apply_knockback(attacker: Node2D):
-	# 방향 : (적의 위치 - 공격자=플레이어 위치)
-	var direction    = (position - attacker.position).normalized()
-	knockback_vector = direction * knockback_strength
-	knockback_time   = knockback_duration
-
-# 접촉 상태가 되었을 때
-func _on_interaction_sensor_body_entered(_body:Node2D):
-	if _body == player and not touch_flag:
-		touch_flag = true
-		# print(touch_flag)
-
-# 접촉 상태에서 벗어날 때
-func _on_interaction_sensor_body_exited(_body:Node2D):
-	if _body == player:
-		touch_flag = false
-	
-func _on_interaction_sensor_area_entered(area:Area2D):
-	if area.is_in_group("attack"):
-		var take_damage = area.get_parent().attack_damage
-		health -= take_damage       # TODO area.damage가 무기 추가 후 각 공격에 맞는 damage가 들어오는지 확인할 필요가 있음
-		DamageVisual.show_damage(take_damage, self.position, Color.WHITE)
-		if health <= 0:
-			die_enemy()
-		else:
-			apply_knockback(area.get_parent())
-			# 데미지 모션 추가
-			hit_flag = true
-			animated_sprite.stop()						# 현재 애니메이션(walk)을 중지시킴
-			animated_sprite.speed_scale = 2.0
-			animated_sprite.play("take_hit")
-			await animated_sprite.animation_finished
-		hit_flag = false
+	super._physics_process(delta)
---
+++ SlayerSurvival/dynamic/2_enemy/base/enemy_base.gd
@@ -0,0 +1,159 @@
+extends CharacterBody2D
+
+class_name EnemyBase
+
+@onready var collision_shape    = $CollisionShape2D
+@onready var animated_sprite    = $AnimatedSprite2D
+@onready var interaction_sensor = $interaction_sensor 
+
+# 아이템
+var gold_img = preload("res://dynamic/6_utillity/items/gold/gold.tscn")
+var exp_img  = preload("res://dynamic/6_utillity/items/exp/exp.tscn")
+
+# 적 특성 (default)
+var enemy_name      = "EnemyBase"
+var health          = 15 	# 적 체력
+var move_speed      = 80 	# 적 이동 속도
+var damage          = 5  	# 적 데미지
+var spawn_radius    = 500  # 적 스폰 범위
+var animation_speed = 1.0 # 적 기본 애니메이션 속도
+var pet_chance      = 0.1 # 적 펫이 될 확률
+# 전역 변수
+var player 
+var touch_flag = false 
+var hit_flag   = false
+var is_dead    = false
+# 넉백 관련
+var knockback_vector   = Vector2.ZERO
+var knockback_time     = 0.0			# 넉백 유지 시간
+var knockback_duration = 0.2			# 넉백 몇 초 동안?
+var knockback_strength = 150.0  		# 넉백 세기
+
+
+func _ready():
+	# player 노드 찾기
+	player = get_parent().get_parent().get_node("player")
+    # 시그널 연결
+	interaction_sensor.connect("area_entered", Callable(self, "_on_interaction_sensor_area_entered"))
+	interaction_sensor.connect("body_entered", Callable(self, "_on_interaction_sensor_body_entered"))
+	interaction_sensor.connect("body_exited" , Callable(self, "_on_interaction_sensor_body_exited"))
+
+func _physics_process(delta):
+	# 사망 상태에서 아무것도 처리 아지 않도록
+	if is_dead:
+		return
+
+	# 넉백 중이면 넉백 로직 우선 적용
+	if knockback_time > 0.0:
+		knockback_time -= delta
+		velocity = knockback_vector
+		move_and_slide()
+		return
+	else:
+		# 넉백 로직 끝날 시 원래 로직으로 복귀귀
+		# 플레이어가 존재하면 플레이어를 향해 이동
+		if player:
+			var direction = (player.position - position).normalized()
+			velocity = direction * move_speed
+			move_and_slide()
+
+		# 애니메이션 처리
+		if (velocity.length() > 0) && (!hit_flag):
+			animated_sprite.speed_scale = animation_speed
+			$AnimatedSprite2D.play("walk")
+			$AnimatedSprite2D.flip_h = velocity.x < 0
+			
+	if touch_flag:
+		player.process_collision_enemy(damage)
+
+
+# 사망 처리 함수
+func die_enemy():
+	var pet_chance_randf = randf()						# 몬스터펫 확률 (0.0~1.0 사이로 조절)
+	is_dead = true 										# 사망 상태 활성화
+	drop_item()
+	player.kill_count += 1
+	collision_shape.call_deferred("set_disabled",true)	# CollisionShape2D 비활성화
+	interaction_sensor.call_deferred("queue_free")		# interaction_sensor 삭제
+	animated_sprite.play("death")
+	await animated_sprite.animation_finished
+	
+	var is_pet = false
+	var pet_dialog_path = ""
+	
+	if enemy_name == "skeleton":
+		is_pet = !player.is_skeleton_pet && pet_chance_randf <= pet_chance
+		if is_pet:
+			player.skeleton_pet = true
+			pet_dialog_path = "res://dynamic/7_dialogic/get_skeleton_pet.dtl"
+	elif enemy_name == "mushroom":
+		is_pet = !player.is_mushroom_pet && pet_chance_randf <= pet_chance
+		if is_pet:
+			player.mushroom_pet = true
+			pet_dialog_path = "res://dynamic/7_dialogic/get_mushroom_pet.dtl"
+	elif enemy_name == "goblin":
+		is_pet = !player.is_goblin_pet && pet_chance_randf <= pet_chance
+		if is_pet:
+			player.goblin_pet = true
+			pet_dialog_path = "res://dynamic/7_dialogic/get_goblin_pet.dtl"
+	elif enemy_name == "flyingeye":
+		is_pet = !player.is_flyingeye_pet && pet_chance_randf <= pet_chance
+		if is_pet:
+			player.flyingeye_pet = true
+			pet_dialog_path = "res://dynamic/7_dialogic/get_flyingeye_pet.dtl"
+			
+	if is_pet:
+		Dialogic.start(pet_dialog_path).process_mode = Node.PROCESS_MODE_ALWAYS
+		Dialogic.process_mode = Node.PROCESS_MODE_ALWAYS
+		
+	queue_free()
+
+# 아이템 드랍 함수
+func drop_item():
+	# 골드
+	var gold_chance = randf()
+	if gold_chance <= 0.5:								# 드랍 확률 조정 (0.0~1.0)
+		var new_gold = gold_img.instantiate()
+		new_gold.global_position = global_position
+		get_parent().call_deferred("add_child", new_gold)
+	# 경험치
+	var exp_chance = randf()
+	if exp_chance <= 0.7:								# 드랍 확률 조정 (0.0~1.0)
+		var new_exp = exp_img.instantiate()
+		new_exp.global_position = global_position + Vector2(10, 0)
+		get_parent().call_deferred("add_child", new_exp)
+
+# 넉백 함수
+func apply_knockback(attacker: Node2D):
+	# 방향 : (적의 위치 - 공격자=플레이어 위치)
+	var direction    = (position - attacker.position).normalized()
+	knockback_vector = direction * knockback_strength
+	knockback_time   = knockback_duration
+
+# 접촉 상태가 되었을 때
+func _on_interaction_sensor_body_entered(_body:Node2D):
+	if _body == player and not touch_flag:
+		touch_flag = true
+		# print(touch_flag)
+
+# 접촉 상태에서 벗어날 때
+func _on_interaction_sensor_body_exited(_body:Node2D):
+	if _body == player:
+		touch_flag = false
+
+func _on_interaction_sensor_area_entered(area:Area2D):
+	if area.is_in_group("attack"):
+		var take_damage = area.get_parent().attack_damage
+		health -= take_damage       # TODO area.damage가 무기 추가 후 각 공격에 맞는 damage가 들어오는지 확인할 필요가 있음
+		DamageVisual.show_damage(take_damage, self.position, Color.WHITE)
+		if health <= 0:
+			die_enemy()
+		else:
+			apply_knockback(area.get_parent())
+			# 데미지 모션 추가
+			hit_flag = true
+			animated_sprite.stop()						# 현재 애니메이션(walk)을 중지시킴
+			animated_sprite.speed_scale = 2.0
+			animated_sprite.play("take_hit")
+			await animated_sprite.animation_finished
+		hit_flag = false
