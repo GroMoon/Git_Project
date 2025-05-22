33b7f40d7ad1dcbf80d1456a4d9b39691f86ff60
GroMoon <msj99112@gmail.com>
2025-05-22 13:58:58 +0900

    === [CHARACTER-041] [BUGFIX] wizard 두번째 공격에 흡혈 안되는 버그

--- SlayerSurvival/dynamic/1_player/characters/Wizard/Lightning/lightning.gd
+++ SlayerSurvival/dynamic/1_player/characters/Wizard/Lightning/lightning.gd
@@ -5,6 +5,7 @@ extends Node2D
 var attack_damage
 
 func _ready():
+	self.name = "player_lightning"
 	attack_damage = get_parent().get_node("player").attack_damage		# 공격력 증가 업그레이드가 player의 damage증가이기 때문에 player를 찾아서 데미지 넣어줌
 	animation_player.play("lightning")
 	await animation_player.animation_finished
