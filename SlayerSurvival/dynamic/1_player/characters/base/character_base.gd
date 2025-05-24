39b773e5af7a94773ea3886dee0637fface8d1f6
wonhs0191 <wonhs0191@naver.com>
2025-05-24 12:51:39 +0900

    vampire 레벨업 적용 최종 (max 값 0.1)

--- SlayerSurvival/dynamic/1_player/characters/base/character_base.gd
+++ SlayerSurvival/dynamic/1_player/characters/base/character_base.gd
@@ -19,10 +19,10 @@ signal levelup
 @export var attack_times        = 1					# 캐릭터 공격 콤보
 @export var shadow_attack       = false				# 캐릭터 그림자 분신술
 @export var attack_damage       = 5					# 캐릭터 일반 공격 데미지
-@export var magnetic_area_scale = 100				# 캐릭터 자석 범위 
+@export var magnetic_area_scale = 100				# 캐릭터 자석 범위
 @export var animation_speed     = 1.0				# 캐릭터 기본 애니메이션 속도
-@export var start_hp            = 100				# 캐릭터 시작 체력
-@export var drain_percent       = 0.0				# 캐릭터 흡혈 퍼센트
+@export var start_hp            = 100.0				# 캐릭터 시작 체력
+@export var vampire             = 0.0				# 캐릭터 흡혈 퍼센트
 
 ## 펫 관련
 # mushroom
@@ -191,7 +191,7 @@ func bind_player_data():
 	attack_damage = attack_damage + int(Global.character_data["CHARACTER_STORE_UPGRADES"]["damage"])
 	move_speed = move_speed + int(Global.character_data["CHARACTER_STORE_UPGRADES"]["speed"])
 	# a = a + int(Global.character_data["CHARACTER_STORE_UPGRADES"]["cooldown"])
-	# a = a + float(Global.character_data["CHARACTER_STORE_UPGRADES"]["vampire"])
+	vampire = vampire + float(Global.character_data["CHARACTER_STORE_UPGRADES"]["vampire"])
 	# a = a + float(Global.character_data["CHARACTER_STORE_UPGRADES"]["gold_drop"])
 	# a = a + float(Global.character_data["CHARACTER_STORE_UPGRADES"]["gem_drop"])
 
@@ -261,17 +261,11 @@ func level_up():
 		emit_signal("levelup")
 
 # 흡혈 능력
-func apply_health(source):
-	# 흡혈 레벨이 0이거나 주체가 자신이 아니라면 무시
-	if drain_level == 0 || source != self:
-		return
-	print(1)
-	var heal = drain_percent * attack_damage
-	if heal < 1:
-		heal = 1
+func apply_health(_source):
+	var heal = vampire * float(attack_damage)
 	current_hp += heal
 	current_hp = clamp(current_hp, 0, max_hp)
-	DamageVisual.show_damage(heal, self.position, Color.GREEN)
+	VampireVisual.show_vampire(heal, self.position, Color.GREEN)
 
 # hit_effect
 func apply_hit_effect():
