58e00c0159a232dac16788c4d5d3d61a3dfc87a3
GroMoon <msj99112@gmail.com>
2025-11-21 14:14:55 +0900

    === 보스 피격 데미지 표시 (붉은색 -> 흰색)

--- SlayerSurvival/dynamic/2_enemy/Boss_FireWorm/fireworm.gd
+++ SlayerSurvival/dynamic/2_enemy/Boss_FireWorm/fireworm.gd
@@ -151,7 +151,7 @@ func _on_interaction_sensor_area_entered(area:Area2D):
 	if area.is_in_group("attack"):
 		var take_damage = area.get_parent().attack_damage
 		health -= take_damage       # TODO area.damage가 무기 추가 후 각 공격에 맞는 damage가 들어오는지 확인할 필요가 있음
-		DamageVisual.show_damage(take_damage, self.position)
+		DamageVisual.show_damage(take_damage, self.position, Color.WHITE)
 		if health <= 0:
 			die_enemy()
 			spawn_portal()
